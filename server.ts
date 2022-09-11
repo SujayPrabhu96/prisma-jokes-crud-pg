966f485b11a0b5496a7cd048f3c90e383e8166dd
SujayPrabhu96 <prabhusujay@gmail.com>
2022-09-11 17:55:21 +0530

    Add endpoint for fetching jokes

--- server.ts
+++ server.ts
@@ -1,8 +1,25 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async(req: Request, res: Response) => {
+  const jokes = await prisma.joke.findMany({
+    include: {creator: true}
+  });
+  res.json({ jokes });
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
+});
+
+app.get("/:joke_id", (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
+});
+
+app.delete("/:joke_id", (req: Request, res: Response) => {
   res.json({ message: "Hello" });
 });
 
