cbd5e040bf1c4f5f6f81bc42860450973a8694d1
SujayPrabhu96 <prabhusujay@gmail.com>
2022-09-11 18:15:36 +0530

    Add endpoint for creating new jokes

--- server.ts
+++ server.ts
@@ -3,24 +3,29 @@ import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const prisma = new PrismaClient();
+app.use(express.json());
 
-app.get("/", async(req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   const jokes = await prisma.joke.findMany({
-    include: {creator: true}
+    include: { creator: true },
   });
   res.json({ jokes });
 });
 
-app.post("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello" });
+app.post("/", async (req: Request, res: Response) => {
+  const { text, userId } = req.body;
+
+  const joke = await prisma.joke.create({
+    data: {
+      text,
+      userId,
+    },
+  });
+  res.json({ joke });
 });
 
-app.get("/:joke_id", (req: Request, res: Response) => {
-  res.json({ message: "Hello" });
-});
+app.get("/:joke_id", (req: Request, res: Response) => {});
 
-app.delete("/:joke_id", (req: Request, res: Response) => {
-  res.json({ message: "Hello" });
-});
+app.delete("/:joke_id", (req: Request, res: Response) => {});
 
-app.listen(3001)
+app.listen(3001);
